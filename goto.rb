550c2c5fd390cb4ea1af5e6a17fc86b2ee0a1e3a
Robin Garen Aaberg <robin.aaberg@knowit.no>
2018-09-11 04:15:16 +0200

    update sha

--- goto.rb
+++ goto.rb
@@ -4,7 +4,7 @@ class Goto < Formula
   desc "A command line tool to make and use magic shortcuts that are project context aware."
   homepage "http://gotogoto.ninja"
   url "https://github.com/technocake/goto/raw/homebrew/archive/goto-0.1.0.tar.gz"
-  sha256 "a27c506f57a1379998e1ad55c647349badf55b9e"
+  sha256 "9699c8cedf78f89dab10ea9924bc84d59345e6f052d66d2a00764c8f3e468d1b"
   version "0.1.0"
 
   bottle :unneeded
