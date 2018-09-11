5863084ad3b45da6ce84a3a3babc3cb3e1da2b70
Robin Garen Aaberg <robin.aaberg@knowit.no>
2018-09-11 04:46:40 +0200

    update install

--- goto.rb
+++ goto.rb
@@ -5,7 +5,7 @@ class Goto < Formula
   homepage "http://gotogoto.ninja"
   url "https://github.com/technocake/goto/raw/homebrew/archive/goto-0.1.0.tar.gz"
   sha256 "9699c8cedf78f89dab10ea9924bc84d59345e6f052d66d2a00764c8f3e468d1b"
-  version "0.1.0"
+  version "0.1.1"
 
   bottle :unneeded
 
